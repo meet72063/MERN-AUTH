cfd402b9ac75a8cfadef1852cf9e545812f9105c
Amit <amitpardhan589@gmail.com>
2024-02-02 18:34:12 +0530

    change app name in the email

--- server/services/email.service.js
+++ server/services/email.service.js
@@ -6,15 +6,15 @@ const createVerificationEmail = (email, verificationToken) => {
 
   return {
     to: email,
-    subject: "Welcome to Skillbridge! Confirm Your Account",
+    subject: "Welcome to MERN-AUTH! Confirm Your Account",
     html: `
         <p>Hello there,</p>
-        <p>Thank you for choosing Skillbridge, your gateway to mastering web development!</p>
-        <p>To complete your registration and unlock access to our high-quality web development courses, please click the link below to verify your account:</p>
+        <p>Thank you for choosing MERN-AUTH</p>
+        <p>To complete your registration , please click the link below to verify your account:</p>
         <p><a href="${verificationLink}" style="text-decoration: none; color: #007bff;">Verify Your Account</a></p>
         <p>This link will expire in 30 minutes for security reasons. If you didn't request this verification, you can safely ignore this email.</p>
         <p>Happy coding!</p>
-        <p>Best regards,<br>The Skillbridge Team</p>
+        <p>Best regards,<br>MERN-AUTH</p>
       `,
   };
 };
@@ -24,46 +24,46 @@ const createResetPasswordEmail = (email, verificationToken) => {
 
   return {
     to: email,
-    subject: "Skillbridge Password Reset",
+    subject: "MERN-AUTH Password Reset",
     html: `
         <p>Hi there,</p>
-        <p>We received a request to change your Skillbridge account password. If this wasn't you, please ignore this email.</p>
+        <p>We received a request to change your MERN-AUTH account password. If this wasn't you, please ignore this email.</p>
         <p>Click the link below to set up a new password. This link will expire in 15 minutes for security reasons:</p>
         <p><a href="${verificationLink}" style="text-decoration: none; color: #007bff;">Change Password</a></p>
         <p>Happy coding!</p>
-        <p>Best regards,<br>The Skillbridge Team</p>
+        <p>Best regards,<br>MERN-AUTH</p>
       `,
   };
 };
 
 const createNewAccountEmail = (email) => {
-  const skillbridgeLink = `http://${process.env.CLIENT_HOST}`;
+  const mernauthlink = `http://${process.env.CLIENT_HOST}`;
 
   return {
     to: email,
-    subject: "Skillbridge Account Verified!",
+    subject: "MERN-AUTH Account Verified!",
     html: `
         <p>Hello,</p>
-        <p>Congratulations! Your email has been successfully verified, and you're now ready to dive into the world of web development with Skillbridge.</p>
+        <p>Congratulations! Your email has been successfully verified</p>
         <h2>Start exploring now:</h2>
-        <p><a href="${skillbridgeLink}" style="text-decoration: none; color: #007bff;">Go to Skillbridge</a></p>
+        <p><a href="${mernauthlink}" style="text-decoration: none; color: #007bff;">Go to MERN-AUTH</a></p>
         <p>Happy coding!</p>
-        <p>Best regards,<br>The Skillbridge Team</p>
+        <p>Best regards,<br>MERN-AUTH</p>
       `,
   };
 };
 const createNewPasswordEmail = (email) => {
-  const skillbridgeLink = `http://${process.env.CLIENT_HOST}`;
+  const mernauthlink = `http://${process.env.CLIENT_HOST}`;
 
   return {
     to: email,
-    subject: "Skillbridge Password Reset Success!",
+    subject: "MERN-AUTH Password Reset Success!",
     html: `
         <p>Hello,</p>
         <p>Congratulations! Your Passowrd has updated Successfully!.Sign In with your new password</p>
-        <p><a href="${skillbridgeLink}" style="text-decoration: none; color: #007bff;">Go to Skillbridge</a></p>
+        <p><a href="${mernauthlink}" style="text-decoration: none; color: #007bff;">Go to MERN-AUTH</a></p>
         <p>Happy coding!</p>
-        <p>Best regards,<br>The Skillbridge Team</p>
+        <p>Best regards,<br>MERN-AUTH</p>
       `,
   };
 };
